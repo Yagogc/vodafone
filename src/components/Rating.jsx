8af1fd0d9662b2aa9da8888d742a9d830bf9e4d4
Yago <jagogc@gmail.com>
2018-03-11 17:31:06 +0000

    Added Star Svg for more browser support

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -15,11 +15,18 @@ class Rating extends Component {
 }
 
 const Star = () => {
-	return <span role='img' aria-label="star">⭐</span>
+	return  (
+		<Svg height="25" width="23">
+			<polygon points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78"/>
+		</Svg>
+	)
 }
 
 export default Rating;
 
 const RatingWrapper = styled.div`
 	margin-bottom: 10px;
+`
+const Svg = styled.svg`
+	fill: #ffd055;
 `
